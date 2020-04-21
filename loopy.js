62c978cf7569761020769ba9570666267ac7517e
Samy0412 <samantha.gadet@gmail.com>
2020-04-21 16:23:00 +0000

    Just loops

---
+++ loopy.js
@@ -0,0 +1,9 @@
+for (let x = 100; x <= 200; x++) {
+  x % 3 === 0 && x % 4 === 0
+    ? console.log("LoopyLighthouse")
+    : x % 3 === 0
+    ? console.log("Loopy")
+    : x % 4 === 0
+    ? console.log("Lighthouse")
+    : console.log(x);
+}
